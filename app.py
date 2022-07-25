8739d5e35477360319ac64f13fb19c1909295c6c
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-25 11:01:59 -0500

    Use dataframe, handle other tsp states

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import subprocess
+import pandas as pd
 from flask import Flask
 from flask import render_template
 from flask import jsonify
@@ -15,24 +16,60 @@ def hello_world():
 def hello():
     return render_template("index.html")
 
-@app.route("/tsp/list")
-@app.route("/tsp/list/<socket_name>")
-def list(socket_name=None):
+def tsp_list(socket_name):
     env = {}
     if socket_name is not None:
         env["TS_SOCKET"] = f"/tmp/socket.{socket_name}"
     output = subprocess.check_output("tsp -l", env=env, shell=True, encoding="utf-8")
-    print(output)
     lines = output.splitlines()
     header_line = lines[0]
     column_names = header_line.split(maxsplit=5)
-    print(column_names)
     assert "Command" in column_names[-1], column_names[-1]
-    lines = lines[1:]
-    lines = [l.split(maxsplit=5) for l in lines]
-    lines_by_header = [dict(zip(column_names, column_datas)) for column_datas in lines]
-    print(json.dumps(lines_by_header, indent=2))
-    return jsonify(lines_by_header)
+    rows = lines[1:]
+    def to_row(line):
+        fields = line.split(maxsplit=5)
+        if fields[1] == "running":
+            fields = line.split(maxsplit=2)
+            long_field = fields[-1]
+            fields = fields[:-1]
+            output, command = long_field.split(maxsplit=1)
+            fields.append(output)
+            fields.append(None)
+            fields.append(None)
+            fields.append(command)
+        if fields[1] == "queued":
+            fields = line.split(maxsplit=2)
+            long_field = fields[-1]
+            fields = fields[:-1]
+            fields.append(long_field[:len("(file)")].strip())
+            fields.append(None)
+            fields.append(None)
+            long_field_rest = long_field[len("(file)"):].strip()
+            fields.append(long_field_rest)
+        if fields[1] == "skipped":
+            fields = line.split(maxsplit=2)
+            long_field = fields[-1]
+            fields = fields[:-1]
+            fields.append(long_field[:len("(no output)")].strip())
+            fields.append(None)
+            fields.append(None)
+            long_field_rest = long_field[len("(no output)"):].strip()
+            fields.append(long_field_rest)
+        return fields
+    rows = [to_row(l) for l in rows]
+    df = pd.DataFrame(data=rows, columns=column_names).set_index("ID")
+    return df
+
+@app.route("/tsp/list")
+@app.route("/tsp/list/<socket_name>")
+def list(socket_name=None):
+    data_by_column = tsp_list(socket_name)
+    data_json = data_by_column.to_json(orient="values")
+    print(data_json)
+    return jsonify(data_json)
 
 def test_list():
-    list()
+    print()
+    # print(tsp_list(None))
+    print(tsp_list("devign"))
+    print(tsp_list("devign").to_json(orient="values"))
