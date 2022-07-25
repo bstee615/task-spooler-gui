34301e6f81b694e35b39f344b8afff6e2f67a7a1
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-25 10:14:58 -0500

    Use jquery and datatables

--- static/index.js
+++ static/index.js
@@ -1,46 +1,10 @@
-let columns = [
-    "ID",
-    "State",
-    "Output",
-    "E-Level",
-    "Times(r/u/s)",
-    "Command [run=0/1]",
-]
-let tableHead = document.getElementById('table-head')
-let tableRow = tableHead.insertRow();
-for (let column of columns) {
-    let tableCell = tableRow.insertCell()
-    tableCell.innerHTML = column
-}
+columns = ["ID", "State", "Output", "E-Level", "Times(r/u/s)", "Command [run=0/1]"]
 
-/*
-
-        <tr>
-          <th>ID</th>
-          <th>State</th>
-          <th>Output</th>
-          <th>E-Level</th>
-          <th>Times(r/u/s)</th>
-          <th>Command</th>
-          <th>[run=0/1]</th>
-        </tr>
-*/
-
-let tableBody = document.getElementById('table-body')
-console.log(tableHead, tableBody)
-
-fetch('/tsp/list')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Success:', data)
-        for (let row of data) {
-            let tableRow = tableBody.insertRow();
-            for (let column of columns) {
-                console.log(row, column, row[column])
-                let tableCell = tableRow.insertCell();
-                tableCell.innerHTML = row[column];
-            }
-        }
-    })
-
-    
+$(document).ready(function () {
+    $.getJSON("/tsp/list", function (data) {
+        $('#mainTable').DataTable({
+            data: data,
+            columns: columns.map((col) => {return { title: col, data: col }})
+        });
+    });
+});
