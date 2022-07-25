17f5f2383d8fa146b0a84df78f49366f2e6b001b
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-25 12:49:42 -0500

    Enable reload with different ajax url

--- static/index.js
+++ static/index.js
@@ -11,57 +11,68 @@ function basename(path) {
     return path.split('/').reverse()[0];
 }
 
-function loadTable() {
+let table = null;
+
+function getAjaxUrl() {
     let socketName = $("#socketName").val();
     let url = "/tsp/list"
     if (socketName != "") {
         url += "/" + socketName
     }
-    console.log(url, socketName);
-    $.getJSON(url, function (data) {
-        $('#mainTable').DataTable({
-            serverSide: true,
-            processing: true,
-            ajax: { url: url },
-            columns: [
-                {
-                    title: "ID",
-                    data: "ID",
+    return url;
+}
+
+function loadTable() {
+    let url = getAjaxUrl();
+    table = $('#mainTable').DataTable({
+        serverSide: true,
+        processing: true,
+        ajax: { url: url },
+        columns: [
+            {
+                title: "ID",
+                data: "ID",
+            },
+            {
+                title: "State",
+                data: "State",
+            },
+            {
+                title: "Output",
+                data: "Output",
+                render: function (data, type, row) {
+                    return '<a href="/tsp/output/' + basename(data) + '">' + data + '</a>';
                 },
-                {
-                    title: "State",
-                    data: "State",
-                },
-                {
-                    title: "Output",
-                    data: "Output",
-                    render: function ( data, type, row ) {
-                        return '<a href="/tsp/output/' + basename(data) + '">' + data + '</a>';
-                    },
-                },
-                {
-                    title: "E-Level",
-                    data: "E-Level",
-                },
-                {
-                    title: "Command",
-                    data: "Command",
-                },
-                {
-                    title: "Time (real)",
-                    data: "Time (real)",
-                },
-                {
-                    title: "Time (user)",
-                    data: "Time (user)",
-                },
-                {
-                    title: "Time (system)",
-                    data: "Time (system)",
-                },
-            ],
-        });
+            },
+            {
+                title: "E-Level",
+                data: "E-Level",
+            },
+            {
+                title: "Command",
+                data: "Command",
+            },
+            {
+                title: "Time (real)",
+                data: "Time (real)",
+            },
+            {
+                title: "Time (user)",
+                data: "Time (user)",
+            },
+            {
+                title: "Time (system)",
+                data: "Time (system)",
+            },
+        ],
     });
 }
 
-$(document).ready(loadTable);
+$(document).ready(function () {
+    // loadSockets();
+    loadTable();
+});
+
+$("#refreshButton").click(function () {
+    table.ajax.url(getAjaxUrl()).load();
+})
