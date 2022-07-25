b7baaedf54654094180aef1660342b1de3bca32d
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-07-25 10:03:53 -0500

    Add task-spooler list job endpoint

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import json
+import subprocess
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -11,3 +14,21 @@ def hello_world():
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/tsp/list")
+def list():
+    output = subprocess.check_output("tsp -l", shell=True, encoding="utf-8")
+    print(output)
+    lines = output.splitlines()
+    header_line = lines[0]
+    column_names = header_line.split(maxsplit=5)
+    print(column_names)
+    assert column_names[-1] == "Command [run=0/1]", column_names[-1]
+    lines = lines[1:]
+    lines = [l.split(maxsplit=5) for l in lines]
+    lines_by_header = [dict(zip(column_names, column_datas)) for column_datas in lines]
+    print(json.dumps(lines_by_header, indent=2))
+    return jsonify(lines_by_header)
+
+def test_list():
+    list()
--- static/index.js
+++ static/index.js
@@ -1,2 +1,46 @@
-let tableBody = document.getElementById("table-body");
-console.log(tableBody)
+let columns = [
+    "ID",
+    "State",
+    "Output",
+    "E-Level",
+    "Times(r/u/s)",
+    "Command [run=0/1]",
+]
+let tableHead = document.getElementById('table-head')
+let tableRow = tableHead.insertRow();
+for (let column of columns) {
+    let tableCell = tableRow.insertCell()
+    tableCell.innerHTML = column
+}
+
+/*
+
+        <tr>
+          <th>ID</th>
+          <th>State</th>
+          <th>Output</th>
+          <th>E-Level</th>
+          <th>Times(r/u/s)</th>
+          <th>Command</th>
+          <th>[run=0/1]</th>
+        </tr>
+*/
+
+let tableBody = document.getElementById('table-body')
+console.log(tableHead, tableBody)
+
+fetch('/tsp/list')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data)
+        for (let row of data) {
+            let tableRow = tableBody.insertRow();
+            for (let column of columns) {
+                console.log(row, column, row[column])
+                let tableCell = tableRow.insertCell();
+                tableCell.innerHTML = row[column];
+            }
+        }
+    })
+
+    
